2ce77ac5c1324ecb6eb6208a19be099f0bac9e80
moksudahmed <k.moksud@gmail.com>
2023-01-22 14:14:30 +0600

    Update Cros

--- server/server.js
+++ server/server.js
@@ -24,6 +24,14 @@ const openai = new OpenAIApi(configuration);
 const app = express()
 app.use(cors())
 app.use(cors({origin: true, credentials: true}));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(express.json())
 
 app.get('/', async (req, res) => {
